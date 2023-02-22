3ef6e17fe5e93a141aae8c8624b97b67c0f97ea6
Bernardo Vera <bvera.ruiz@gmail.com>
2023-02-22 18:17:24 +0100

    update2.3

--- services/movies.services.js
+++ services/movies.services.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const API_KEY = process.env.API_KEY
 
 class MovieApi {
 
@@ -11,13 +12,13 @@ class MovieApi {
     // Search Movie by ID..for api tryou
 
     getMovie = (id) => {
-        return this.api.get(`/movie/${id}?api_key=6368d7de1031af85856eeb00f048afbf&language=en-US`).then(({ data }) => data)
+        return this.api.get(`/movie/${id}?api_key=${API_KEY}&language=en-US`).then(({ data }) => data)
     }
 
     // Search movie by year, rate and genre
 
     getRandom = (year, rate, genre) => {
-        return this.api.get(`/discover/movie?api_key=6368d7de1031af85856eeb00f048afbf&language=en-US&sort_by=vote_average.desc&page=1&primary_release_year=${year}&vote_count.gte=500&vote_average.gte=${rate}&with_genres=${genre}`).then(({ data }) => data)
+        return this.api.get(`/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&page=1&primary_release_year=${year}&vote_count.gte=500&vote_average.gte=${rate}&with_genres=${genre}`).then(({ data }) => data)
     }
 }
 
