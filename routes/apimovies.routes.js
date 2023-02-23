63ffa91401dc16f0804cba8ea82f21c05a20bca9
alvmorsie <122047484+alvmorsie@users.noreply.github.com>
2023-02-23 12:01:05 +0100

    done

--- routes/apimovies.routes.js
+++ routes/apimovies.routes.js
@@ -71,9 +71,20 @@ router.post('/recommendations/:movie_id', isLoggedIn, (req, res, next) => {
 
     User
         .findByIdAndUpdate(user_id, { $addToSet: { recommendations: movie_id } })
-        .then(() => res.redirect(`/movie/list-random`))
+        .then(() => res.redirect('/movie/search-random'))
         .catch(err => next(err))
 
+
+})
+
+router.post('/removeFromFav/:movie_id', isLoggedIn, (req, res, next) => {
+    const { movie_id } = req.params
+    const user_id = req.session.currentUser._id
+
+    User
+        .findByIdAndUpdate(user_id, { $pull: { recommendations: movie_id } })
+        .then(() => res.redirect(`/user/${user_id}`))
+        .catch(err => next(err))
 
 })
 
